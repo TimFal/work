60ae3c6a36ca1b673a280edb662bd6df401ab27c
JuiceJupeach <48614605+JuiceJupeach@users.noreply.github.com>
2019-04-25 21:57:48 +0300

    Update caesar_crypt.py

--- caesar_crypt.py
+++ caesar_crypt.py
@@ -1,7 +1,13 @@
 def caesar_crypt_word(string):
-    crypted = ""
+    
+    string = string.lower() #приводим нашу строку к нижнему регистру
+    crypted = "" #создаем строку crypted 
+    
     for i in range(len(string)):
-        crypted += str(chr(ord(string[i]) + 5))
-    return crypted
+        
+        crypted += str(chr(ord(string[i]) + 5)) #к пустой строке crypted добовляем символы из string следующие за ними через 5
+        
+    return crypted #возвращаем строку crypted
+
 word = input()
 print(caesar_crypt_word(word))
